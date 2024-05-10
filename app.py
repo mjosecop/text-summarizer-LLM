3ceb05fd9c8c380664ab8314f2da8a4274431052
mjosecop <128254116+mjosecop@users.noreply.github.com>
2024-05-10 20:02:52 +0200

    app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
 from langchain.chains.summarize import load_summarize_chain
-from langchain import PromptTemplate
-from langchain.llms import CTransformers
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
